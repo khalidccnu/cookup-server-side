95eb361fb44506505e945e32472fa41a79b263d5
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-02 15:25:36 +0600

    chefs connected

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const chefs = require("./asset/chefs.json");
+
+app.get("/", (req, res) => {
+    res.send("CookUp is running...");
+});
+
+app.get("/chefs", (req, res) => {
+    res.send(chefs);
+});
+
 app.listen(port, () => {
     console.log(`CookUp API is running on port: ${port}`);
 });
