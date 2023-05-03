abca62bd86fc5333c436d51d0bbb8c6962eb8592
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-03 08:46:18 +0600

    recipes added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const chefs = require("./asset/chefs.json");
+const recipes = require("./asset/recipes.json");
 
 app.get("/", (req, res) => {
     res.send("CookUp is running...");
@@ -18,6 +19,11 @@ app.get("/chefs", (req, res) => {
 app.get("/chefs/:id", (req, res) => {
     const chef = chefs.find(chef => chef.id === +req.params.id);
     res.send(chef);
+});
+
+app.get("/chef-recipes/:id", (req, res) => {
+    const recipesData = recipes.filter(recipe => recipe["chef_id"] === +req.params.id);
+    res.send(recipesData);
 });
 
 app.listen(port, () => {
